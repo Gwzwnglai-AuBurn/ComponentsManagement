96c908b76d724f266546f5ec6051e75a089e0c4d
Gwzwnglai-AuBurn <gwzwnglai@protonmail.com>
2020-08-21 13:44:43 +0530

    Initial commit

---
+++ app/src/main/java/io/gwzwn/managelibrary/Book_location.java
@@ -0,0 +1,24 @@
+package io.gwzwn.managelibrary;
+
+/**
+ * Created by Administrator on 12/05/2017.
+ */
+
+public class Book_location {
+
+    public String booklocation;
+
+
+    public Book_location(String booklocation) {
+        this.booklocation = booklocation;
+    }
+
+    public Book_location()
+    {
+
+    }
+
+    public String getbooklocation() {
+        return booklocation;
+    }
+}
